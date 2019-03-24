c488369616594f5e4800d3cbcf0a51b61cd3cf7a
Jorge Hernández <jorge1231239@hotmail.com>
2019-03-24 18:30:27 -0400

    Same captcha for a single page
    I had a problem wanting to use this tool on a single page, for which I was forced to edit it, this change will make the image always be the same in all forms on the same page, later I will improve it to allow different images inside from the same page, it was a quick adjustment, greetings.

--- src/resources/views/vendor/bone/captcha/image.blade.php
+++ src/resources/views/vendor/bone/captcha/image.blade.php
@@ -1,6 +1,27 @@
-<img src="{{ $route }}"
-     alt="https://github.com/igoshev/laravel-captcha"
+<img src=""
+     alt=""
      style="cursor:pointer;width:{{ $width }}px;height:{{ $height }}px;"
      title="{{ $title }}"
-     onclick="this.setAttribute('src','{{ $route }}?_='+Math.random());var captcha=document.getElementById('{{ $input_id }}');if(captcha){captcha.focus()}"
+     class="captcha"
+     onclick="renewCaptcha()"
 >
+
+@section("scripts")
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            renewCaptcha();
+        });
+        function renewCaptcha() {
+            var captchas = document.getElementsByClassName("captcha");
+            var new_image = '{{ $route }}?_=' + Math.random();
+            for (let i = 0; i < captchas.length; i++) {
+                const element = captchas[i];
+                element.setAttribute('src', new_image);
+            }
+            var captcha = document.getElementById('{{ $input_id }}');
+            if (captcha) {
+                captcha.focus();
+            }
+        }
+    </script>
+@endsection
